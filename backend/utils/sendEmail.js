d8c0d8d968e11254c24fcf3974bbf04836007901
venhartconceptstore <ivkovickorp@gmail.com>
2026-03-23 13:35:50 +0100

    Fix email SMTP config

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,11 +3,14 @@ const nodemailer = require('nodemailer');
 const createTransporter = () => {
   return nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false,
+    port: parseInt(process.env.EMAIL_PORT) || 465,
+    secure: process.env.EMAIL_PORT === '465' ? true : false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
     }
   });
 };
